d6ae54d9b4bd6c52927fe910e2b5c1d28912d1e3
Joel Wong Jun Qin <joel.wong@student.gamudaai.academy>
2025-10-02 05:33:59 +0000

    Feat/frontend/add profile settings

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Header username="Loser" />
+        <Header username="User" notifications={3} />
         {children}
         <Footer />
       </body>
