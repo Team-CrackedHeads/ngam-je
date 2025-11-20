374fb1cba036c4e04a5ab1e2f06db52556611786
Yuvarani Jagathesan <yuvaranij463@gmail.com>
2025-11-21 01:46:11 +0800

    fixed the alembic migration error

--- backend/alembic/versions/54bdf29d8110_add_kyc_session_token_field.py
+++ backend/alembic/versions/54bdf29d8110_add_kyc_session_token_field.py
@@ -13,7 +13,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = '54bdf29d8110'
-down_revision: Union[str, Sequence[str], None] = '9b439cc5f433'
+down_revision: Union[str, Sequence[str], None] = '5023e7edb906'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
